02f81da671ec7828774e62e109e02a04e2236890
Ashish Singh <ashishcbscboard@gmail.com>
2021-01-22 11:49:28 +0530

    ChatApp v1.0.0

---
+++ public/index.js
@@ -0,0 +1,62 @@
+const socket = io("http://localhost:3000");
+
+const form = document.getElementById('text-mess');
+const messageinput = document.getElementById('message-input');
+const roomContainer = document.getElementById('room-container');
+const broadcast = document.querySelector('.message-container');
+
+var audio = new Audio('../public/Receive.mp3');
+
+// append function 
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    broadcast.append(messageElement);
+    if (position == 'left') {
+        audio.play();
+    }
+}
+
+if (broadcast != null) {
+    while (true) {
+        const Username = prompt("Enter you name", "Anonymous");
+        if (Username == null) {
+        alert('Name is necessery');
+        }else{
+            socket.emit("new-user-joined", roomName,Username);
+            break;
+        }
+    }
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = messageinput.value;
+        append(`You: ${message}`, 'right');
+        socket.emit('send', roomName,message);
+        messageinput.value = '';
+    });
+}
+
+socket.on('room-created',room =>{
+    const roomElement = document.createElement('div');
+    roomElement.innertext = room;
+    const roomLink = document.createElement('a');
+    roomLink.classList.add("room-join");
+    roomLink.href = `/${room}`;
+    roomLink.innerText = 'join'
+    roomContainer.append(roomElement);
+    roomContainer.append(roomLink);
+});
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right');
+});
+
+socket.on('receive', data => {
+    append(`${data.name} : ${data.message}`, 'left');
+});
+
+socket.on('left', name => {
+    append(`${name} left the chat`, 'left');
+});
---
+++ server.js
@@ -0,0 +1,127 @@
+// getting express
+const express = require('express');
+// getting mongoose for database entries  
+const mongoose = require('mongoose');
+// setting connection with mongo database
+mongoose.connect('mongodb://localhost:27017/chatapp', { useNewUrlParser: true, useUnifiedTopology: true });
+//getting connection of database as db variable
+const db = mongoose.connection;
+// logging error if occurs
+db.on('error', console.error.bind(console, 'connection error:'));
+// checking for once are we connected or not
+db.once('open', function () {
+    // we're connected!
+    console.log("We are connected!");
+});
+// making schema
+const roomdata = new mongoose.Schema({
+    room: String,
+    password: String,
+});
+// converting schema into model to use it in db
+const roominfo = mongoose.model('roomdata', roomdata);
+// announcing it as express app
+const app = express();
+// creating server with the help of http
+const server = require('http').Server(app)
+// getting socket.io in express server 
+const io = require('socket.io')(server)
+// serving views file so it can be accessed by server
+app.set('views', './views');
+// setting view engine
+app.set('view engine', 'ejs');
+// serving static files so it can be acces by server
+app.use(express.static('public'));
+// getting values from client sites
+app.use(express.urlencoded({ extended: true }))
+// room varible to handle rooms
+const rooms = {}
+// rendering home page
+app.get('/', (req, res) => {
+    res.render('index', { rooms: rooms });
+});
+// rendering login page
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+// posting room page,getting values from client site and saving room datas into database
+app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+        return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} }
+    const data = {
+        room : req.body.room,
+        password : req.body.roomPassword,
+    }
+    const roomdata = new roominfo(data);
+    roomdata.save();
+    res.redirect(req.body.room)
+    // send message
+    io.emit('room-created', req.body.room);
+});
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+        return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room });
+});
+// serving rooms and finding in databse 
+app.post('/getroom',(req,res)=>{
+    const getdata ={
+        room : req.body.room,
+        password : req.body.roomPassword,
+    }
+    roominfo.find(getdata,(err,data)=>{
+        if (err) {
+            console.log(err);
+        }
+        if (data == "") {
+            return res.redirect('/login')
+        }
+        res.redirect(req.body.room)
+    })
+})
+// making server
+server.listen(3000, () => {
+    console.log('App Started on port 3000');
+});
+// socket connections
+io.on('connection', (socket) => {
+    // New user connection
+    socket.on('new-user-joined', (room, name) => {
+        socket.join(room);
+        rooms[room].users[socket.id] = name
+        socket.to(room).broadcast.emit('user-joined', name);
+    });
+    // sending and receiving message
+    socket.on('send', (room, message) => {
+        socket.to(room).broadcast.emit('receive', { message: message, name: rooms[room].users[socket.id] })
+    });
+    // handling disconnection and deleting rooms form databases
+    socket.on('disconnect', (room, name) => {
+        getUserName(socket).forEach(room => {
+            socket.to(room).broadcast.emit('left', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id];
+            if (Object.keys(rooms[room].users).length === 0) {
+                const droom = {
+                    room:room
+                }
+                roominfo.deleteOne(droom,(err)=>{
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        delete rooms[room]
+                    }
+                })
+            }
+        })
+    })
+})
+// function to get username of client who just disconnected
+function getUserName(socket) {
+    return Object.entries(rooms).reduce((names, [roomname, room]) => {
+        if (room.users[socket.id] != null) names.push(roomname)
+        return names
+    }, [])
+}
